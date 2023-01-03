5f07019218eb6f2470dc068fe7b5e106cb5f36eb
Mehmet ONAT <mehmetonatce@gmail.com>
2023-01-03 04:45:43 +0300

    initial
    initial files

---
+++ CoreCRUD/Models/CoreCRUDContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace CoreCRUD.Models
+{
+    public class CoreCRUDContext :DbContext
+    {
+        public CoreCRUDContext(DbContextOptions<CoreCRUDContext>options): base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }//Student tablosu
+        public DbSet<Teacher> Teachers { get; set; }//Teacher tablosu
+
+    }
+}
---
+++ CoreCRUD/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreCRUD.Models
+{
+    public class Teacher
+    {
+
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Department { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
